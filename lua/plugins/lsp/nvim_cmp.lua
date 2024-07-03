774f55b3665c05b609baf5d5e15cc65acb62fa7a
TheFieryLynx <biryukov.andrej2000@mail.ru>
2024-07-03 23:22:21 +0300

    change config for nvim-cmp

--- lua/plugins/lsp/nvim_cmp.lua
+++ lua/plugins/lsp/nvim_cmp.lua
@@ -4,6 +4,8 @@ return {
   },
   {
     'L3MON4D3/LuaSnip',
+    version = "v2.*",
+    build = "make install_jsregexp",
     dependencies = {
       'saadparwaiz1/cmp_luasnip',
       'rafamadriz/friendly-snippets',
@@ -20,17 +22,20 @@ return {
     config = function()
       require("luasnip.loaders.from_vscode").lazy_load()
       local cmp = require('cmp')
-
       cmp.setup({
         snippet = {
           expand = function(args)
             require('luasnip').lsp_expand(args.body)
           end,
         },
+        completion = {
+          completeopt = "menu,menuone,preview,noselect",
+        },
         window = {
           completion = cmp.config.window.bordered(),
           documentation = cmp.config.window.bordered(),
         },
+
         mapping = cmp.mapping.preset.insert({
           ['<C-b>'] = cmp.mapping.scroll_docs(-4),
           ['<C-f>'] = cmp.mapping.scroll_docs(4),

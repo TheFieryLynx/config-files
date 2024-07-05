a1216b22c3abde666e2aa32bbdea67ae4e5480b1
TheFieryLynx <biryukov.andrej2000@mail.ru>
2024-07-05 23:31:38 +0300

    pyright setup WIP

--- lua/plugins/lsp/mason_lspconfig.lua
+++ lua/plugins/lsp/mason_lspconfig.lua
@@ -1,50 +1,73 @@
+local function find_venv(path)
+	print(path)
+	local possible_env_names = {}
+	possible_env_names[1] = "/.env"
+	possible_env_names[2] = "/.venv"
+	possible_env_names[3] = "/env"
+	possible_env_names[4] = "/venv"
+
+	for _, v in ipairs(possible_env_names) do
+		local possible_path = path .. v
+		if vim.fn.isdirectory(possible_path) == 1 then
+			print("Found python environment: ", possible_path)
+			return possible_path
+		end
+	end
+	print("Python environment not found!")
+end
+
+local function pyright_on_init()
+	local cwd = vim.fn.getcwd()
+	local current_venv = find_venv(cwd)
+end
+
 return {
-  "williamboman/mason-lspconfig.nvim",
-  dependencies = { 'mason.nvim', 'hrsh7th/nvim-cmp' },
+	"williamboman/mason-lspconfig.nvim",
+	dependencies = { "mason.nvim", "hrsh7th/nvim-cmp" },
 
-  config = function()
-    local mason_lspconfig = require("mason-lspconfig")
-    mason_lspconfig.setup({})
+	config = function()
+		local mason_lspconfig = require("mason-lspconfig")
+		mason_lspconfig.setup({})
 
-    local capabilities = require('cmp_nvim_lsp').default_capabilities()
+		local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-    require("mason-lspconfig").setup_handlers({
-      function(server_name)
-        require("lspconfig")[server_name].setup({})
-      end,
+		require("mason-lspconfig").setup_handlers({
+			function(server_name)
+				require("lspconfig")[server_name].setup({})
+			end,
 
+			["ansiblels"] = function()
+				local lspconfig = require("lspconfig")
+				lspconfig.ansiblels.setup({
+					filetypes = { "yaml", "yml" },
+					capabilities = capabilities,
+				})
+			end,
 
-      ["ansiblels"] = function()
-        local lspconfig = require("lspconfig")
-        lspconfig.ansiblels.setup({
-          filetypes = { "yaml", "yml" },
-          capabilities = capabilities,
-        })
-      end,
+			["pyright"] = function()
+				local lspconfig = require("lspconfig")
+				lspconfig.pyright.setup({
+					capabilities = capabilities,
+					filetypes = { "python" },
+					on_init = function(client)
+						return pyright_on_init()
+					end,
+				})
+			end,
 
-      ["pyright"] = function()
-        local lspconfig = require("lspconfig")
-        lspconfig.pyright.setup({
-          capabilities = capabilities,
-        })
-      end,
-
-
-      ["lua_ls"] = function()
-        local lspconfig = require("lspconfig")
-        lspconfig.lua_ls.setup({
-          capabilities = capabilities,
-          settings = {
-            Lua = {
-              diagnostics = {
-                globals = { 'vim' },
-              },
-            },
-          },
-        })
-      end
-
-
-    })
-  end
+			["lua_ls"] = function()
+				local lspconfig = require("lspconfig")
+				lspconfig.lua_ls.setup({
+					capabilities = capabilities,
+					settings = {
+						Lua = {
+							diagnostics = {
+								globals = { "vim" },
+							},
+						},
+					},
+				})
+			end,
+		})
+	end,
 }

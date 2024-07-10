a1ea0d1a7a229c7dba4260cce09e5f60139af791
TheFieryLynx <biryukov.andrej2000@mail.ru>
2024-07-11 00:14:11 +0300

    add new header and 'IDE' name

--- nvim/lua/user-settings.lua
+++ nvim/lua/user-settings.lua
@@ -2,7 +2,7 @@ local system_settings = require("system-params")
 local ascii_headers = require("plugins.assets.ascii")
 
 local user_settings = {
-  dashboard_header = ascii_headers.header2,
+  dashboard_header = ascii_headers.header3,
   theme = system_settings.available_themes.nord,
   transparent_background = true,
 }

463fe60ae39cf4ddb7cc465e9c04b8062fb1609a
TheFieryLynx <biryukov.andrej2000@mail.ru>
2024-07-11 22:57:54 +0300

    configure comment plugin

--- nvim/lua/plugins/comments.lua
+++ nvim/lua/plugins/comments.lua
@@ -3,9 +3,14 @@ return {
   config = function()
     local comment = require("Comment")
     comment.setup({
-      options = {
-        padding = true,
-      },
+      padding = true,
+      sticky = true,
     })
+
+    vim.keymap.set("v", "<leader>cc", "<Plug>(comment_toggle_linewise_visual)", { desc = "Comment visual lines" })
+    vim.keymap.set("v", "<leader>cb", "<Plug>(comment_toggle_blockwise_visual)", { desc = "Comment visual block" })
+
+    vim.keymap.set("n", "<leader>cb", "<Plug>(comment_toggle_blockwise_current)", { desc = "Comment line block" })
+    vim.keymap.set("n", "<leader>cc", "<Plug>(comment_toggle_linewise_current)", { desc = "Comment line current" })
   end,
 }

01a1a8a0620b989761765d30c4c32bfebc4aeead
Andrew Biryukov <biryukov.andrej2000@ispras.ru>
2024-07-13 14:11:51 +0300

    change background to transparent

--- nvim/lua/user-settings.lua
+++ nvim/lua/user-settings.lua
@@ -4,7 +4,7 @@ local ascii_headers = require("plugins.assets.ascii")
 local user_settings = {
   dashboard_header = ascii_headers.header3,
   theme = system_settings.available_themes.catppuccin,
-  transparent_background = false,
+  transparent_background = true,
 }
 
 return user_settings

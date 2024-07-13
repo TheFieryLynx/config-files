40b34f9ba30b05cb8613d98c2be3c0f2aa9e298b
Andrew Biryukov <biryukov.andrej2000@ispras.ru>
2024-07-13 14:06:19 +0300

    fix which-key version

--- nvim/lua/plugins/which-key.lua
+++ nvim/lua/plugins/which-key.lua
@@ -1,5 +1,6 @@
 return {
   "folke/which-key.nvim",
+  tag = "v2.1.0",
   event = "VeryLazy",
   init = function()
     vim.o.timeout = true
@@ -9,12 +10,12 @@ return {
     local whichkey = require("which-key")
     whichkey.setup({
       options = {
-        mode = "n", -- NORMAL mode
+        mode = "n",     -- NORMAL mode
         prefix = "<leader>",
-        buffer = nil, -- Global mappings. Specify a buffer number for buffer local mappings
-        silent = true, -- use `silent` when creating keymaps
+        buffer = nil,   -- Global mappings. Specify a buffer number for buffer local mappings
+        silent = true,  -- use `silent` when creating keymaps
         noremap = true, -- use `noremap` when creating keymaps
-        nowait = true, -- use `nowait` when creating keymaps
+        nowait = true,  -- use `nowait` when creating keymaps
       },
     })
   end,

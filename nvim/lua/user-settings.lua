1d000b432af07794e96e379642d85bfc83d3143f
Andrew Biryukov <biryukov.andrej2000@mail.ru>
2024-08-26 23:22:46 +0300

    add vim and wallpaper style

--- nvim/lua/user-settings.lua
+++ nvim/lua/user-settings.lua
@@ -2,7 +2,7 @@ local system_settings = require("system-params")
 local ascii_headers = require("plugins.assets.ascii")
 
 local user_settings = {
-  dashboard_header = ascii_headers.header3,
+  dashboard_header = ascii_headers.header4,
   theme = system_settings.available_themes.catppuccin,
   transparent_background = true,
 }

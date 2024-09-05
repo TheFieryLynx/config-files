b4ad93a01a88c45e7f688fcdf21ebb3c206029d6
Andrew Biryukov <biryukov.andrej2000@mail.ru>
2024-09-05 23:05:07 +0300

    add gruvbox theme

--- alacritty/change_theme.sh
+++ alacritty/change_theme.sh
@@ -9,6 +9,8 @@ if [[ $1 == "catppuccin" ]] then
   cp ./catppuccin.toml ./CURRENT_THEME.toml
 elif [[ $1 == "nord" ]] then
   cp ./nord.toml ./CURRENT_THEME.toml
+elif [[ $1 == "gruvbox" ]] then
+  cp ./gruvbox.toml ./CURRENT_THEME.toml
 fi
 
 touch ./alacritty.toml

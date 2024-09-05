42900f7b6a8affefa3fe5214fa31170e924af9ae
Andrew Biryukov <biryukov.andrej2000@mail.ru>
2024-09-05 23:01:17 +0300

    add script for changing theme

---
+++ alacritty/change_theme.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [[ $# -eq 0 ]]; then
+  >&2 echo "Specify theme name (catppuccin|nord)"
+    exit 1
+fi
+
+if [[ $1 == "catppuccin" ]] then
+  cp ./catppuccin.toml ./CURRENT_THEME.toml
+elif [[ $1 == "nord" ]] then
+  cp ./nord.toml ./CURRENT_THEME.toml
+fi
+
+touch ./alacritty.toml
